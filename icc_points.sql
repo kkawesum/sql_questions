07633a35ef9e6a8431378b7294eb7636eda8529c
Kislay Srivastava <KislayS@mymiratech.com>
2023-03-22 12:52:32 +0530

    day 2 sql playlist

--- icc_points.sql
+++ icc_points.sql
@@ -11,3 +11,15 @@ INSERT INTO icc_world_cup values('Eng','NZ','NZ');
 INSERT INTO icc_world_cup values('Aus','India','India');
 
 select * from icc_world_cup;
+----------------------------------DML--------------------------------------------
+
+select team_name,count(1) as matches_played,sum(wins) as matches_won from
+
+(
+select team_1 as team_name,case when team_1=winner then 1 else 0 end as wins from icc_world_cup 
+union all
+
+select team_2 as team_name,case when team_2=winner then 1 else 0 end as wins from icc_world_cup )
+group by team_name
+order by matches_won desc
+;
